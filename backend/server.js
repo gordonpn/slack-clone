499f17cdd23f10579497e2a5be740852d71f39d5
Mackbellemore <43047923+Mackbellemore@users.noreply.github.com>
2019-09-18 14:24:31 -0400

    Adding back the database connection

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-/*
+
 const db_uri = process.env.ATLAS_URI;
-mongoose.connect(db_uri, { useNewUrlParser: true});
+mongoose.connect(db_uri, {  useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("Database connection success");
 });
-*/
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
