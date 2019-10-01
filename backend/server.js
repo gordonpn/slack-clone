69ce8b996e245b97f5e2fa012d60edee5adf7adf
gpnn <gordon.pn6@gmail.com>
2019-10-01 13:21:55 -0400

    #6. some improvements to the schemas

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,16 @@ app.use(cors());
 app.use(express.json());
 
 const db_uri = process.env.ATLAS_URI;
-mongoose.connect(db_uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('Database connection success');
+mongoose.connect(db_uri,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+const db_connection = mongoose.connection;
+db_connection.once('open', () => {
+    console.log('Database connection success');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
