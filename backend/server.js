0aae946092931426bc9e8cdfd1e5ef85f4750dcc
Mackbellemore <43047923+Mackbellemore@users.noreply.github.com>
2019-10-01 21:16:57 -0400

    #13 added endpoints for users

--- backend/server.js
+++ backend/server.js
@@ -12,20 +12,23 @@ app.use(express.json());
 
 const db_uri = process.env.ATLAS_URI;
 mongoose
-    .connect(db_uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log('Successfully connected to the database');
-    })
-    .catch(err => {
-        console.log('Could not connect to the database. Exiting now...', err);
-        process.exit();
-    });
+  .connect(db_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('Successfully connected to the database');
+  })
+  .catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+  });
 
 require('./routes/channels.routes.js')(app);
+require('./routes/users.routes.js')(app);
 
 app.listen(port, () => {
-    console.log('Server is running on port: ${port}');
+  console.log(`Server is running on port: ${port}`);
 });
