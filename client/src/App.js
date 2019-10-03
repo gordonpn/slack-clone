0e487a240fc655cf74fdd643276d292aca2857a3
Mackbellemore <43047923+Mackbellemore@users.noreply.github.com>
2019-10-02 20:11:09 -0400

    #24 initial login page ui with axios calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import ChatDashboard from "./components/ChatDashboard";
 import Login from "./components/Login";
 import "./index.css";
@@ -7,14 +8,71 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: null,
-      isLoggedIn: true
+      user: { username: "", id: "", friendIDs: [], channelIDs: [] },
+      isLoggedIn: false
     };
     this.setUser = this.setUser.bind(this);
+    this.logUserIn = this.logUserIn.bind(this);
   }
-  setUser = userChild => {
-    this.setState({ user: userChild, isLoggedIn: true });
+  async setUser(e, userName) {
+    e.preventDefault();
+    await axios.post('/users', {
+      username: userName
+    })
+      .then(response => {
+        const newUser = {
+          username: userName,
+          id: response.data['_id'],
+          friendIDs: response.data['friendIDs'],
+          channelIDs: response.data['channelIDs']
+        }
+        this.setState({
+          user: newUser,
+          isLoggedIn: true
+        })
+      })
+      .catch(error => {
+        if (error.response) {
+          console.log(error.data.message);
+          alert("username already exits")
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+      });
   };
+
+  async logUserIn(e, userName) {
+    e.preventDefault();
+
+    await axios.get(`/users/username/${userName}`)
+      .then(response => {
+        // handle success
+        console.log("successful login");
+
+        const user = {
+          username: userName,
+          id: response.data['_id'],
+          friendIDs: response.data['friendIDs'],
+          channelIDs: response.data['channelIDs']
+        }
+        this.setState({
+          user: user,
+          isLoggedIn: true
+        })
+
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error.response.data);
+      })
+  }
+
   render() {
     return (
       <div className="container-fluid">
@@ -22,14 +80,14 @@ export default class App extends Component {
           <h1>Slack Clone App</h1>
         </div>
         {!this.state.isLoggedIn ? (
-          <Login setUser={this.setUser} />
+          <Login logUserIn={this.logUserIn} />
         ) : (
-          <ChatDashboard
-            user={this.state.user}
-            isloggedIn={this.state.isLoggedIn}
-            name="mack"
-          />
-        )}
+            <ChatDashboard
+              user={this.state.user}
+              isloggedIn={this.state.isLoggedIn}
+              name="mack"
+            />
+          )}
       </div>
     );
   }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ username: e.target.value });
+  }
+
   render() {
     return (
       <div>
         <form>
           <label>
             {" "}
-            username
-            <input type="text" />
-            password
-            <input type="password" />
-            <button> log me in</button>
+            username:
+            <input type="text" value={this.state.username} onChange={this.handleChange} />
+            <button onClick={e => this.props.logUserIn(e, this.state.username)}> log me in</button>
           </label>
         </form>
       </div>
