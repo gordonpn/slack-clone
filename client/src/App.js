c7375e3c60cd7f7b37746a92a066de4314b2c835
Mackbellemore <43047923+Mackbellemore@users.noreply.github.com>
2019-10-08 12:14:10 -0400

    #42 refactored app.js to put logic in componentDidMount

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import ChatDashboard from "./components/ChatDashboard";
 import Login from "./components/Login";
-import { getUserByName, addUser } from "./api/users";
+import {getUserByName, addUser} from "./api/users";
 import Button from "react-bootstrap/Button";
 import "./index.css";
 
 export default class App extends Component {
   constructor() {
     super();
-    const isLoggedIn = localStorage.getItem('isLoggedIn') ? true : false;
-    const user = localStorage.getItem('user');
     this.state = {
-      user: JSON.parse(user) || { username: "", id: "", friendIDs: [], channelIDs: [] },
-      isLoggedIn: isLoggedIn,
+      user: {username: "", id: "", friendIDs: [], channelIDs: []},
+      isLoggedIn: false,
       isLoading: false
     };
     this.setUser = this.setUser.bind(this);
     this.logUserIn = this.logUserIn.bind(this);
     this.logOutUser = this.logOutUser.bind(this);
+  }
+  async componentDidMount() {
+    const isLoggedIn = localStorage.getItem('isLoggedIn') ? true : false;
+    if (isLoggedIn) {
+      try {
+        let userName = localStorage.getItem('userName');
+        let user = await this.getUser(userName);
+
+        this.setState({
+          isloggedIn: true,
+          user: user
+        })
+      } catch (err) {
+        console.log(err);
+      }
+    }
   }
 
   async setUser(userName) {
@@ -34,23 +48,13 @@ export default class App extends Component {
 
   async logUserIn(userName) {
     try {
-      const response = await getUserByName(userName);
-      if (response.data.length === 0) {
-        console.log("invalid username");
-        return
-      }
-      const newUser = {
-        username: userName,
-        id: response.data[0]._id,
-        friendIDs: response.data[0].friendIDs,
-        channelIDs: response.data[0].channelIDs
-      }
+      const user = await this.getUser(userName)
       this.setState({
-        user: newUser,
+        user: user,
         isLoggedIn: true
       })
       localStorage.setItem('isLoggedIn', this.state.isLoggedIn);
-      localStorage.setItem('user', JSON.stringify(this.state.user));
+      localStorage.setItem('userName', JSON.stringify(this.state.user.username));
     } catch (error) {
       console.log(error);
       return;
@@ -62,16 +66,35 @@ export default class App extends Component {
     })
     localStorage.clear();
   }
+
+  async getUser(userName) {
+    try {
+      const response = await getUserByName(userName);
+      if (response.data.length === 0) {
+        console.log("invalid username");
+        return
+      }
+      const newUser = {
+        username: userName,
+        id: response.data[0]._id,
+        friendIDs: response.data[0].friendIDs,
+        channelIDs: response.data[0].channelIDs
+      }
+      return newUser;
+    } catch (err) {
+      throw err;
+    }
+  }
   render() {
     return (
       <div className="container-fluid">
         <div className="jumbotron-fluid" align="center">
           <h1>Slack Clone App</h1>
-            <Button
-              type="button"
-              className="logoutButton btn btn-secondary"
-              onClick={this.logOutUser}>
-              Log out
+          <Button
+            type="button"
+            className="logoutButton btn btn-secondary"
+            onClick={this.logOutUser}>
+            Log out
           </Button>
         </div>
         {!this.state.isLoggedIn ? (
