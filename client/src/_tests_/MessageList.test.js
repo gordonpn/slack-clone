1b30fdd6945912718e2762d8ceaedfb3e71319c5
Mackenzie Bellemore <43047923+Mackbellemore@users.noreply.github.com>
2019-10-31 10:20:06 -0400

    #10 ples travis

--- client/src/_tests_/MessageList.test.js
+++ client/src/_tests_/MessageList.test.js
@@ -6,12 +6,12 @@ require('../setupTests');
 describe('MessageList component should render messages', () => {
   it('renders messageList with empty messages without crashing', () => {
     const messages = [];
-    shallow(<MessageList messages={messages}/>);
+    shallow(<MessageList messages={messages} />);
   });
 
   it('renders messageList with 1 message without crashing', () => {
     const messages = [];
-    shallow(<MessageList messages={messages}/>);
+    shallow(<MessageList messages={messages} />);
   });
 
 });
