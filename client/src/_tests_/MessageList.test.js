8b06956ca9531297be38242e54bf03a4ce2e5674
Mackenzie Bellemore <43047923+Mackbellemore@users.noreply.github.com>
2019-10-31 10:50:40 -0400

    #10 work now

--- client/src/_tests_/MessageList.test.js
+++ client/src/_tests_/MessageList.test.js
@@ -6,12 +6,14 @@ require('../setupTests');
 describe('MessageList component should render messages', () => {
   it('renders messageList with empty messages without crashing', () => {
     const messages = [];
-    shallow(<MessageList messages={messages} />);
+    const wrapper = shallow(<MessageList messages={messages} />);
   });
 
   it('renders messageList with 1 message without crashing', () => {
-    const messages = [];
-    shallow(<MessageList messages={messages} />);
+    const messages = [{senderId: 111, parts: [{payload: {content: "hello"}}]}];
+    const wrapper = shallow(<MessageList messages={messages} />);
+    expect(wrapper.find('div')).toBeTruthy();
+    console.log(wrapper.debug())
   });
 
 });
