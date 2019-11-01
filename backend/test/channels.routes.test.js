5a642b9baafe66370326ce5a8b3eca5397a9b7aa
gordonpn <gordon.pn6@gmail.com>
2019-10-31 21:06:12 -0400

    #10 added some more basic back-end unit tests. increased the timeout of mocha to 10 secs (this is ugly and is not good)

--- backend/test/channels.routes.test.js
+++ backend/test/channels.routes.test.js
@@ -11,35 +11,23 @@ const expect = require('chai').expect;
 const should = require('chai').should;
 const request = require('supertest');
 
-describe('testing get route for users list', () => {
+describe('testing get route for channels list', () => {
   it('should return 200 status', () => {
     return request(app)
-      .get('/users')
+      .get('/channels')
       .then( (response) => {
         assert.equal(response.status, 200)
       })
   });
 });
 
-describe('testing get route for a non-existent user id', () => {
+describe('testing get route for a non-existent channel id', () => {
   it('should return the 404 status', () => {
     let id = "idNotFound";
     return request(app)
-      .get(`/users/${id}`)
+      .get(`/channel/${id}`)
       .then((response) => {
         assert.equal(response.status, 404)
       })
   })
 });
-
-describe('testing get route for a non-existent user name', () => {
-  it('should return the 200 status', () => {
-    let username = "nameNotFound";
-    return request(app)
-      .get(`/users/username/${username}`)
-      .then((response) => {
-        assert.equal(response.status, 200)
-      })
-  })
-});
-
